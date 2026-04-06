bed67098f9e2d8e75ef0e56f5561b8668157a535
bellayuliasetiawan <bellayuliasetiawan@gmail.com>
2026-04-06 14:09:47 +0700

    Add files via upload

---
+++ PERTUMUAN2/latihan2/src/latihan2/latihan2.java
@@ -0,0 +1,22 @@
+package latihan2;
+import java.util.Scanner;
+
+public class latihan2 
+{
+    public static void main(String args[]) 
+    {
+        Scanner input = new Scanner(System.in);
+        String nama, nim, address;
+
+        System.out.print("input nama: ");
+        nama = input.nextLine();
+
+        System.out.print("input nim: ");
+        nim = input.nextLine();
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+
+        
+    }
+}
